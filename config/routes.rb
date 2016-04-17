954c704591ee773cc46a72104b590ce3e424a716
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-17 01:12:50 -0400

    Updated user profile page and made name field visible in user new page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [ :show, :create, :destroy ]
   end
 
-  resources :users, only: [ :new, :create ]
+  resources :users, only: [ :new, :create, :show, :edit, :update ]
   resources :sessions, only: [ :new, :create, :destroy ]
 end
